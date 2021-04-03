181c8d3cd7e5afcda7af57de6e79b6daf0aae12e
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-03 18:35:45 -0300

    testando no vscode

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+        System.out.println("Olá Contweb");
+
+    }
+}
